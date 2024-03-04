c51e35ba76e62e23c417572fd533a32af5070dcb
EmineKilik <99184470+EmineKilik@users.noreply.github.com>
2024-03-04 17:23:38 +0300

    FeedYourAnimals

--- Assets/Scripts/MoveForward.cs
+++ Assets/Scripts/MoveForward.cs
@@ -11,7 +11,7 @@ public class MoveForward : MonoBehaviour
         
     }
 
-    //Ýleriye doðru hareket saðlar.
+    //Ä°leriye doÄŸru hareket saÄŸlar.
     void Update()
     {
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
